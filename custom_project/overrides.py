66b0a655e1c3f837b0c3bd1cee6dfce151351a52
Zach <zacharysalvatore1@gmail.com>
2023-08-18 09:47:06 +0800

    origin method

--- custom_project/overrides.py
+++ custom_project/overrides.py
@@ -4,20 +4,4 @@ from frappe.model.document import Document
 from erpnext.projects.doctype.project.project import Project
 class CustomProject(Project):
 	def create_task_from_template(self, task_details):
-		return frappe.get_doc(
-			dict(
-				doctype="Task",
-				subject=task_details.subject,
-				project=self.name,
-				status="Open",
-				exp_start_date=self.calculate_start_date(task_details),
-				exp_end_date=self.calculate_end_date(task_details),
-				description=task_details.description,
-				task_weight=task_details.task_weight,
-				type=task_details.type,
-				issue=task_details.issue,
-				is_group=task_details.is_group,
-				color=task_details.color,
-				task_user= task_details.task_user,
-			)
-		).insert()
+		super().create_create_task_from_template(task_details)
