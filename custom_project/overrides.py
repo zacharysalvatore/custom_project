065735a71ba66d237d588bac4cec775d767fe630
Zach <zacharysalvatore1@gmail.com>
2023-08-21 10:05:52 +0800

    move function content to subclass

--- custom_project/overrides.py
+++ custom_project/overrides.py
@@ -4,4 +4,19 @@ from frappe.model.document import Document
 from erpnext.projects.doctype.project.project import Project
 class CustomProject(Project):
 	def create_task_from_template(self, task_details):
-		super().create_create_task_from_template(task_details)
+		return frappe.get_doc(
+			dict(
+				doctype="Task",
+				subject=task_details.subject,
+				project=super.name,
+				status="Open",
+				exp_start_date=super.calculate_start_date(task_details),
+				exp_end_date=super.calculate_end_date(task_details),
+				description=task_details.description,
+				task_weight=task_details.task_weight,
+				type=task_details.type,
+				issue=task_details.issue,
+				is_group=task_details.is_group,
+				color=task_details.color,
+			)
+		).insert()
